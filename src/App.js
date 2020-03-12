1cb46d89e51288f809bcb13858551c7d6d05476c
saifur703 <saifur703@gmail.com>
2020-03-12 22:44:15 +0600

    Component and State

--- src/App.js
+++ src/App.js
@@ -1,24 +1,113 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const person = {
+    name: 'Saifur',
+    age: 24,
+    job: 'Freelancing'
+  };
+  const products = [
+    {
+      title: 'Web Design',
+      description: 'lorem ipsum ammet here',
+      btnLink: 'https://www.webdesign.com',
+      btnText: 'Learn More'
+    },
+    {
+      title: 'Photoshop',
+      description: 'Lorem ipsum photoshop text',
+      btnText: 'Learn More',
+      btnLink: 'https://photoshop.com'
+    },
+    {
+      title: 'SEO',
+      description: 'hello SEO here lorem ipsum',
+      btnLink: 'link.com',
+      btnText: 'Go to PS'
+    }
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <h2 style={{ color: 'red', backgroundColor: 'green' }}>
+          My Name is: {person.name}
+        </h2>
+        <h3>My age is: {person.age}</h3>
+        <h4>{`My Job is: ${person.job}`}</h4>
+
+        <Counter></Counter>
+
+        <div className='product-area'>
+          <Product product={products[0]}></Product>
+          <Product product={products[1]}></Product>
+          <Product product={products[2]}></Product>
+        </div>
+
+        <div className='person-area'>
+          <Person name='Saifur' age='24' />
+          <Person name='Mamun' age='14' />
+          <Person name='Akash' age='15' />
+        </div>
       </header>
+    </div>
+  );
+}
+
+function Product(props) {
+  const productStyle = {
+    border: '1px solid #ddd',
+    background: '#777',
+    padding: '15px 30px',
+    margin: '15px',
+    width: '20%',
+    float: 'left'
+  };
+
+  // Destructuring
+  const { title, description, btnText, btnLink } = props.product;
+  return (
+    <div style={productStyle}>
+      <h3>{title}</h3>
+      <p>{description}</p>
+      <a href={btnLink}>{btnText}</a>
+    </div>
+  );
+}
+
+function Person(props) {
+  const personStyle = {
+    border: '1px solid red',
+    margin: '10px',
+    padding: '10px 20px',
+    float: 'left'
+  };
+  return (
+    <div style={personStyle}>
+      <h3>My Name is: {props.name}</h3>
+      <p>My age is {props.age}</p>
+    </div>
+  );
+}
+
+function Counter() {
+  const [count, setCount] = useState(110);
+  const handleIncrease = () => {
+    const increment = count + 1;
+    setCount(increment);
+  };
+  const handleDecrease = () => {
+    const decrement = count - 1;
+    setCount(decrement);
+  };
+  return (
+    <div>
+      Counter: {count}
+      <br />
+      <button onClick={handleIncrease}>Increase</button>
+      <button onClick={handleDecrease}>Decrease</button>
     </div>
   );
 }
